2695c842d22d02b146b3cf70fc6639a5c5d76941
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2023-03-07 18:29:59 +0300

    Messages send to queue by Default exchange

--- RabbitMQExchangeTypes.publisher/Program.cs
+++ RabbitMQExchangeTypes.publisher/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hmdtzelt:1GHaeNWNZBVBux7YQFkpa_74fee5eLwY@hawk.rmq.cloudamqp.com/hmdtzelt");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var queueName = "hello-queue";
+channel.QueueDeclare(queueName, true, false, false);
+
+string message = "hello world";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+Console.WriteLine("Mesaj gönderilmiştir");
+
+Console.ReadLine();
+
--- RabbitMQExchangeTypes.subscriber/Program.cs
+++ RabbitMQExchangeTypes.subscriber/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hmdtzelt:1GHaeNWNZBVBux7YQFkpa_74fee5eLwY@hawk.rmq.cloudamqp.com/hmdtzelt");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+var queueName = "hello-queue";
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: queueName, true, consumer);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Mesaj alındı: {message}");
+};
+
+
+Console.ReadLine();
