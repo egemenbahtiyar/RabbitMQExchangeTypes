db4ddac6e368ab15e1015abd5f8ecf024fefa9ac
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2023-03-08 01:49:40 +0300

    Direct Exchange implemented.

--- RabbitMQExchangeTypes.publisher/Program.cs
+++ RabbitMQExchangeTypes.publisher/Program.cs
@@ -1,23 +1,50 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
 
-var factory = new ConnectionFactory();
-factory.Uri = new Uri("amqps://hmdtzelt:1GHaeNWNZBVBux7YQFkpa_74fee5eLwY@hawk.rmq.cloudamqp.com/hmdtzelt");
-
-using var connection = factory.CreateConnection();
-var channel = connection.CreateModel();
-
-var exhangeName = "hello-fanout-exchange";
-channel.ExchangeDeclare(exhangeName, ExchangeType.Fanout, true, false, null);
-
-Enumerable.Range(0, 100).ToList().ForEach(i =>
+public enum LogNames
 {
-    var message = $"hello world {i}";
-    var messageBody = Encoding.UTF8.GetBytes(message);
-    channel.BasicPublish(exhangeName, string.Empty, null, messageBody);
-});
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
 
-Console.WriteLine("Mesaj gönderilmiştir");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://hmdtzelt:1GHaeNWNZBVBux7YQFkpa_74fee5eLwY@hawk.rmq.cloudamqp.com/hmdtzelt");
 
-Console.ReadLine();
+        using var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        var exhangeName = "hello-direct-exchange";
+        channel.ExchangeDeclare(exhangeName, ExchangeType.Direct, true, false, null);
+
+        Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+        {
+            var routeKey = $"route-{x}";
+            var queueName = $"direct-queue-{x}";
+            channel.QueueDeclare(queueName, true, false, false);
+
+            channel.QueueBind(queueName, exhangeName,routeKey,null);
+
+        });
+
+        Enumerable.Range(0, 100).ToList().ForEach(i =>
+        {
+            LogNames logName = (LogNames) new Random().Next(1, 5);
+            var message = $"{logName} - {i}";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            var routeKey = $"route-{logName}";
+            channel.BasicPublish(exhangeName, routeKey, null, messageBody);
+            Console.WriteLine($"Log gönderilmiştir : {message}");
+        });
+
+        Console.WriteLine("Mesaj gönderilmiştir...");
+
+        Console.ReadLine();
+    }
+}
 
--- RabbitMQExchangeTypes.subscriber/Program.cs
+++ RabbitMQExchangeTypes.subscriber/Program.cs
@@ -8,17 +8,15 @@ factory.Uri = new Uri("amqps://hmdtzelt:1GHaeNWNZBVBux7YQFkpa_74fee5eLwY@hawk.rm
 using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
-var randomQueueName = channel.QueueDeclare().QueueName;
 
-channel.QueueBind(randomQueueName, "hello-fanout-exchange", string.Empty, null);
 var consumer = new EventingBasicConsumer(channel);
 
-channel.BasicQos(0,1,false);
-channel.BasicConsume(queue: randomQueueName, false, consumer);
+channel.BasicQos(0,2,false);
+channel.BasicConsume("direct-queue-Info", false, consumer);
 
 consumer.Received += (model, ea) =>
 {
-    Thread.Sleep(500);
+    Thread.Sleep(2000);
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
     Console.WriteLine($"Mesaj alındı: {message}");
